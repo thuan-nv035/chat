64588b57f97fc5d0c951a498f45fb693893f723e
thuan-nv035 <54436704+thuan-nv035@users.noreply.github.com>
2022-04-21 17:02:00 +0700

    update

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -47,7 +47,7 @@ const Login = () => {
 									className="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
 									id="email"
 									type="text"
-									placeholder="Username"
+									placeholder="email"
                                     onChange={(e) => setEmail(e.target.value)} 
                                     value = {email}
 								/>
